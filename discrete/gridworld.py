05be98612d61304e5063586a413bac7956912d62
Felippe Roza <felippesroza@gmail.com>
2020-12-17 15:45:17 +0100

    gridworld with obstacle working

---
+++ discrete/gridworld.py
@@ -0,0 +1,103 @@
+import pygame
+import sys
+import random
+import time
+
+BLACK =     (  0,   0,   0)
+WHITE =     (250, 250, 250)
+BLUE =      (  0,   0, 255)
+GREEN =     (  0, 255,   0)
+RED =       (255,   0,   0)
+
+
+class actor(object):
+    """docstring for actor"""
+    def __init__(self, blocksize = 40, HEIGHT = 10, WIDTH = 10, x0 = 0, y0 = 0, actor_type = 'obstacle'):
+        super(actor, self).__init__()
+        self.x = x0
+        self.y = y0
+        self.h = HEIGHT
+        self.w = WIDTH
+        self.step_size = 1 # how many tiles it moves each tick
+        self.blocksize = blocksize
+        self.type = actor_type
+
+    def move(self):
+        options = ['up', 'down', 'right', 'left']
+        action = random.choice(options)
+
+        if action == 'up':
+            self.y = min(self.y + self.step_size, self.h - 1)
+        elif action == 'down':
+            self.y = max(self.y - self.step_size, 0) # avoid negative coordinate
+        elif action == 'right':
+            self.x = min(self.x + self.step_size, self.w - 1)
+        else:
+            self.x = max(self.x - self.step_size, 0)
+        print('coordinate: ', self.x, self.y)
+
+    def draw(self, screen):
+        pos = (self.x * self.blocksize + self.blocksize/2, self.y * self.blocksize + self.blocksize/2)
+        if self.type == 'obstacle':
+            color = RED
+        elif self.type == 'agent':
+            color = BLUE
+        elif self.type == 'goal':
+            color = GREEN
+        pygame.draw.circle(screen, color, pos, self.blocksize / 2)
+
+
+class gridWorld(object):
+    """docstring for gridWorld"""
+    def __init__(self, HEIGHT = 10, WIDTH = 10, blocksize = 40):
+        super(gridWorld, self).__init__()
+        
+        self.h = HEIGHT # number of vertical blocks
+        self.w = WIDTH  # number of horizontal blocks
+        self.blocksize = blocksize # size of the grid block
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.w * self.blocksize, self.h * self.blocksize))
+        self.clock = pygame.time.Clock()
+        self.goal = actor(actor_type = 'goal')
+        self.obstacle = actor(x0 = 5, y0 = 5)
+        self.run()
+
+    def run(self):
+        
+        run = True
+
+        while(run):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            self.screen.fill(WHITE)
+            self.drawGrid()
+            self.tick()
+            pygame.display.flip()
+
+        self.quit()
+
+    def quit(self):
+        pygame.display.quit()
+        pygame.quit()
+        sys.exit()
+
+    def tick(self):
+        self.obstacle.move()
+        self.obstacle.draw(self.screen)
+        time.sleep(1)
+
+
+    def drawGrid(self):
+        for x in range(self.h):
+            for y in range(self.w):
+                rect = pygame.Rect(x*self.blocksize, y*self.blocksize,
+                                   self.blocksize, self.blocksize)
+                pygame.draw.rect(self.screen, BLACK, rect, 1)
+
+
+
+if __name__ == "__main__":
+    
+    game = gridWorld()

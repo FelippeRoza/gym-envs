97fcf5f507e65c21f77eb41843e1e787f06b6400
Felippe Roza <felippesroza@gmail.com>
2021-04-11 21:20:23 +0200

    adapted manual_control to new render

--- manual_control.py
+++ manual_control.py
@@ -1,28 +1,33 @@
 import gym
 import grid_envs
 
-
 def key_event(event):
 
-	if event.key == 'up':
-		action = 0
-	elif event.key == 'down':
-		action = 1
-	elif event.key == 'left':
-		action = 2
-	elif event.key == 'right':
-		action = 3
-	else:
-		print('invalid key, will go up')
-		action = 0
-	s_t, r_t, done, _ = env.step(action)
-	if done:
-		print('Done! Reward:', r_t)
-		env.reset()
+    if event.key == 'up':
+        action = 0
+    elif event.key == 'down':
+        action = 1
+    elif event.key == 'left':
+        action = 2
+    elif event.key == 'right':
+        action = 3
+    else:
+        print('invalid key, will go up')
+        action = 0
+    s_t, r_t, done, _ = env.step(action)
+    print(s_t)
+    if done:
+        print('Done! Reward:', r_t)
+        env.reset()
 
-	env.render()
+    env.render()
+
+def on_close(event):
+    print('Press [enter] to end')
 
 env = gym.make("DynamicGridWorld-2-MovObs-7x7-Random-v0")
-env.fig.canvas.mpl_connect('key_press_event', key_event)
 env.reset()
-env.render()
+
+env.fig.canvas.mpl_connect('key_press_event', key_event)
+env.fig.canvas.mpl_connect('close_event', on_close)
+input("Press [enter] to continue.")
